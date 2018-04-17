f648d9a3806112c16724d4d3f3393ce43a10adfd
Sandeep Ramgolam <sandeep@lsl.digital>
2018-04-18 03:07:43 +0400

    generated simple WC with fetch

---
+++ components/MyApp.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+      <h1>Hello {{name}}</h1>
+      <input type="text" v-model="name">
+      <div :key="item.id" v-for="item in message">
+        <img :src="item.avatar" alt="">
+        {{ item.first_name}}
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "Sandeep",
+      image: "",
+      message: ""
+    };
+  },
+  mounted() {
+    fetch("https://reqres.in/api/users?page=2")
+      .then(response => {
+        return response.json();
+      })
+      .then(myJson => {
+        this.message = myJson.data;
+      });
+  }
+};
+</script>
